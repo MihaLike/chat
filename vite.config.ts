0b7cc861d8c4e9467db38b53a2927bd90ffcbf50
Михаил Андреевич Окмянский <o_MOkmyanskiy@sportmaster.ru>
2025-08-01 21:40:10 +0300

    feat: доработал настройки проекта (алиасы, настройки тс)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+    vue(),
+    dts({
+      outDir: 'dist',
+      rollupTypes: true, // Склеивает всё в один файл index.d.ts
+      include: ['src/**/*.ts', 'src/**/*.vue'], // Поддержка Vue и TypeScript
+      exclude: ['node_modules', 'dist'],
+    }),
+  ],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@helpers': fileURLToPath(new URL('./src/helpers', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
 		},
+    extensions: ['.vue', '.ts', '.js', '.json'],  
 	},
 	base: '/chat/',
 });
+  
